09a6e6a7c6b0a284a5616ef3e7e331d0ff2e4931
nomisum <nomisum@googlemail.com>
2016-09-29 22:12:49 +0200

    first commit not yet working

---
+++ loadouts.hpp
@@ -0,0 +1,13 @@
+#include ".\list_macros.hpp"
+#include "..\USERSCRIPTS\loadouts.sqf"
+#define FACTIONIZE(var) class O_##var: B_##var {}; class I_##var: B_##var {};
+
+class Loadouts {
+    baseDelay = 1;
+    handleRadios = 1;
+
+    #include "factions\BW.hpp"
+    #include "factions\US.hpp"
+    #include "factions\RUS.hpp"
+    #include "factions\Rebels.hpp"
+};
